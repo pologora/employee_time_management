8e09fd09ffd78c70e7aa2a159e7fdc9b4549956a
Oleksandr <lysakova@yahoo.com>
2023-03-19 19:16:58 +0100

    simple demo calendar with events

--- frontend/src/components/pages/vacations/Calendar.jsx
+++ frontend/src/components/pages/vacations/Calendar.jsx
@@ -1,6 +1,89 @@
-import FullCalendar from '@fullcalendar/react'; // must go before plugins
-import dayGridPlugin from '@fullcalendar/daygrid'; // a plugin!
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
 
-export default function Calendar() {
-  return <FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth" />;
+const urlop = [
+  {
+    type: 'wypoczynkowy',
+    start: '2023-03-21',
+    end: '2023-03-25',
+    name: 'Michał Kowalski',
+    title: 'Michał Kowalski wypoczynkowy',
+    allDay: true,
+  },
+  {
+    type: 'wypoczynkowy',
+    start: '2023-03-21',
+    end: '2023-03-23',
+    name: 'Michał Kowalski',
+    title: 'Lina Białowa wypoczynkowy',
+    allDay: true,
+  },
+  {
+    type: 'wypoczynkowy',
+    start: '2023-03-21',
+    end: '2023-03-29',
+    name: 'Michał Kowalski',
+    title: 'Ola Kulemina wypoczynkowy',
+    allDay: true,
+  },
+  {
+    type: 'wypoczynkowy',
+    start: '2023-02-02',
+    end: '2023-02-12',
+    name: 'Kris Moel',
+    title: 'Kris Moel wypoczynkowy',
+    allDay: true,
+  },
+  {
+    type: 'zwolnienie lekarskie',
+    start: '2023-03-03',
+    end: '2023-03-08',
+    name: 'Ola Wierzyczka',
+    title: 'Ola Wierzyczka zwolnienie lekarskie',
+    allDay: true,
+  },
+  {
+    type: 'wolny za sobotę',
+    start: '2023-03-15',
+    end: '2023-03-16',
+    name: 'Ola Wierzyczka',
+    title: 'Ola Wierzyczka wolny za sobotę',
+    allDay: true,
+  },
+];
+
+function Calendar() {
+  const handleDateClick = (arg) => {
+    // Handle date click event here, e.g. show a modal for adding a new event
+    alert(`Date clicked: ${arg.dateStr}`);
+  };
+
+  const handleEventClick = (arg) => {
+    // Handle event click event here, e.g. show a modal with event details
+    alert(`Event clicked: ${arg.event.title}`);
+  };
+
+  return (
+    <div className="calendar">
+      <FullCalendar
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+        headerToolbar={{
+          left: 'prev,next today',
+          center: 'title',
+          right: 'dayGridMonth,timeGridWeek,timeGridDay',
+        }}
+        initialView="dayGridMonth"
+        events={urlop}
+        dateClick={handleDateClick}
+        eventClick={handleEventClick}
+        selectable
+        editable
+        eventResizableFromStart
+      />
+    </div>
+  );
 }
+
+export default Calendar;
--- frontend/src/components/pages/vacations/Vacations.jsx
+++ frontend/src/components/pages/vacations/Vacations.jsx
@@ -1,8 +1,12 @@
+import { Box, Button } from '@mui/material';
 import Calendar from './Calendar';
 
 function Urlop() {
   return (
     <div>
+      <Box>
+        <Button>Dodaj</Button>
+      </Box>
       <Calendar />
     </div>
   );
