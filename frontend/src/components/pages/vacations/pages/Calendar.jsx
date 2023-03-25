1036606d1f9b555d5def2a559c656617540c7bf0
Oleksandr <lysakova@yahoo.com>
2023-03-25 11:43:17 +0100

    get vacations for calendar

--- frontend/src/components/pages/vacations/pages/Calendar.jsx
+++ frontend/src/components/pages/vacations/pages/Calendar.jsx
@@ -1,33 +1,21 @@
-/* eslint-disable no-underscore-dangle */
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import { CircularProgress } from '@mui/material';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import useAxios from '../../../../hooks/useAxios';
 import baseUrl from '../../../../options/baseUrl';
-
-// const urlop = [
-//   {
-//     type: 'wypoczynkowy',
-//     start: '2023-03-21',
-//     end: '2023-03-25',
-//     name: 'Michał Kowalski',
-//     title: 'Michał Kowalski wypoczynkowy',
-//     allDay: true,
-//   },
-// ];
+import vacationTypes from '../../../../options/vacationTypes';
+import './Calendar.css';
 
 function Calendar() {
   const [events, setEvents] = useState(null);
   const { get, isLoading } = useAxios();
 
-  const handleDatesSet = async (arg) => {
-    const { start, end } = arg;
-    const url = `${baseUrl}/vacations/time?start=${start.toISOString()}&end=${end.toISOString()}`;
+  const getVacations = async () => {
+    const url = `${baseUrl}/vacations/time`;
     const data = await get(url);
-
     const dataForEvents = data?.map((doc) => {
       const {
         _id: id,
@@ -39,6 +27,9 @@ function Calendar() {
         surname,
         duration,
       } = doc;
+      const backgroundColor = vacationTypes.find(
+        (item) => item.label === type,
+      ).color;
       return {
         type,
         start: startVacation,
@@ -50,19 +41,28 @@ function Calendar() {
         duration,
         employeeId,
         id,
+        backgroundColor,
       };
     });
     setEvents(dataForEvents);
   };
 
-  const handleDateClick = (arg) => {
-    // Handle date click event here, e.g. show a modal for adding a new event
-    alert(`Date clicked: ${arg.dateStr}`);
-  };
+  useEffect(() => {
+    getVacations();
+  }, []);
 
   const handleEventClick = (arg) => {
     // Handle event click event here, e.g. show a modal with event details
     alert(`Event clicked: ${arg.event.title}`);
+    console.log(arg);
+  };
+
+  const dayCellClassNames = ({ date }) => {
+    const day = date.getDay();
+    if (day === 0 || day === 6) {
+      return ['weekend'];
+    }
+    return [];
   };
 
   return isLoading ? (
@@ -75,14 +75,14 @@ function Calendar() {
           left: 'prev,next',
           center: 'title',
         }}
-        datesSet={handleDatesSet}
         initialView="dayGridMonth"
         events={events}
-        dateClick={handleDateClick}
         eventClick={handleEventClick}
         selectable
+        firstDay={1}
         editable={false}
         eventResizableFromStart
+        dayCellClassNames={dayCellClassNames}
       />
     </div>
   );
