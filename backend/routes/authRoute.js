0848ed5b3e6ce9d32677ac516df08eb7735422df
Oleksandr <lysakova@yahoo.com>
2023-04-09 07:42:45 +0200

    delete mobile and backend folders

--- backend/routes/authRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { createAdmin, login } = require('../controllers/authController');
-
-const router = express.Router();
-
-router.route('/login').post(login);
-router.route('/').post(createAdmin);
-
-module.exports = router;
