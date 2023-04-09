62eb65e10a71349a70e83635766ea3a09d48d1bf
Oleksandr <lysakova@yahoo.com>
2023-04-09 08:03:07 +0200

    dlete

--- mobile/options.js
+++
@@ -1,7 +0,0 @@
-// const stage = 'development';
-const stage = 'production';
-
-const APP_ID = stage === 'development' ? 'test-hbegu' : 'snti-ewidencja-lrfni';
-const BASE_URL = 'https://realm.mongodb.com';
-
-export {APP_ID, BASE_URL};
