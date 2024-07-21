5f88f032db54c85613aae53ef1a2c41b27ed92b8
Oleksandr <lysakova@yahoo.com>
2024-07-21 11:42:44 +0200

    fix: update dependencies

--- frontend/src/options/axiosInstance.js
+++ frontend/src/options/axiosInstance.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://magazyn-demo-30f1f8a297d3.herokuapp.com/api/v1';
+// const BASE_URL = 'https://magazyn-demo-30f1f8a297d3.herokuapp.com/api/v1';
+// const BASE_URL = 'https://admin.sntiapp.stronawcal.pl/api/v1';
+const BASE_URL = 'https://magazyn-server.webdevolek.stronawcal.pl/api/v1';
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
--- mobile_app/options.js
+++ mobile_app/options.js
@@ -1,7 +1,8 @@
 const stage = 'development';
 // const stage = 'production';
+// const oldDomain = 'https://realm.mongodb.com'
 
 const APP_ID = stage === 'development' ? 'test-hbegu' : 'snti-ewidencja-lrfni';
-const BASE_URL = 'https://realm.mongodb.com';
+const BASE_URL = 'https://services.cloud.mongodb.com';
 
 export {APP_ID, BASE_URL};
